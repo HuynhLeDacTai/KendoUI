47714e874b25ee12018cde901f00ba3a467f663b
HuynhLeDacTai <dactai2k2@gmail.com>
2024-07-30 17:01:27 +0700

    Change JSON to modelMap

--- src/main/java/com/example/demothymeleaf/controllers/UserController.java
+++ src/main/java/com/example/demothymeleaf/controllers/UserController.java
@@ -5,11 +5,13 @@ import com.example.demothymeleaf.Dtos.UserRequestDto;
 import com.example.demothymeleaf.models.Log;
 import com.example.demothymeleaf.models.User;
 import com.example.demothymeleaf.services.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,21 +32,24 @@ public class UserController {
 
     @GetMapping("/users")
     @ResponseBody
-    public List<User> getUsers() {
-        logger.info("User list", userService.getAllUsers());
-        return userService.getAllUsers();
+    public String getUsers(ModelMap model) {
+        logger.info("User list : {}", userService.getAllUsers());
+        model.addAttribute("users", userService.getAllUsers());
+        return "index";
     }
 
     @PostMapping("/users")
     @ResponseBody
-    public void addUser(@RequestParam UserRequestDto user) {
-        logger.debug("Add user", user);
+    public void addUser(@RequestParam("user") UserRequestDto user, HttpServletRequest request) {
+        logger.info("Servlet rq: {}", request);
+        logger.info("Add user: {}", user);
         userService.addUser(user);
     }
 
     @DeleteMapping("/users")
     @ResponseBody
     public void deleteUser(@RequestParam("id") Long id) {
+        logger.info("Delete user: {}", id);
         userService.deleteUser(id);
     }
 }
